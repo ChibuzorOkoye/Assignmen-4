815682f28764337a93a7d6e52b9a23ffb6ba368a
Paul Okoye <Chibuzorokoye77@gmail.com>
2022-01-07 08:14:42 -0500

    Just leaving this here for now

---
+++ button.js
@@ -0,0 +1,15 @@
+btn = document.getElementById("#btn")
+
+function btn1()
+{
+    btn.innerHTML('I am Right')
+} 
+
+
+Btn2 = document.getElementById(".btn")
+
+function btn2()
+{
+    Btn2.onclick = Btn2.innerHTML('No!! I am Right')
+} 
+
